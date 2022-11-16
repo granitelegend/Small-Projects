e27877ff331c9906c45314d6967c228b75edbd28
granitelegend <83376235+granitelegend@users.noreply.github.com>
2022-11-16 03:18:35 +0100

    added comments, improved code quality

--- Next Prime Number.py
+++ Next Prime Number.py
@@ -4,12 +4,14 @@
 # Planned:
 # Twin Primes
 
+"""This file contains simple functions for checking primes"""
+
 import math
 
 def prime_checker(number):
     """
     Checks if a number is prime
-    :param number: (positive integer)
+    :param int
     :return: boolean
     """
 
@@ -24,33 +26,29 @@ def prime_checker(number):
 
     # Simple for-loop that divides number with divisor and checks for remainder
 
-    for divisorNum in range(2, math.ceil(math.sqrt(number + 1))):  # Checks all factors lower than ceil(sqrt(number))
-        if number % divisorNum == 0:
+    # Checks all factors lower than ceil(sqrt(number))
+    for divisor_num in range(2, math.ceil(math.sqrt(number + 1))):
+        if number % divisor_num == 0:
             return False
-            break
-        else:
-            continue
 
     return True
 
 def next_prime():
     """
     Prints the next prime in order of positive integers when user asks for it
+    :parameter: None
     :return: None
     """
 
-    a = 1
+    is_prime_number = 1
     answer_input = 0
 
     while answer_input != 'n':
-        if prime_checker(a):
-            print(a)
-            a +=1
+        if prime_checker(is_prime_number):
+            print(is_prime_number)
+            is_prime_number +=1
             answer_input = input("Generate next prime? Enter anything but 'n' to continue: ")
         else:
-            a += 1
-            continue
-
-next_prime()
+            is_prime_number += 1
 
 
